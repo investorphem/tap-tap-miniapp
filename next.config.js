bbc61ece6cd3e155c4196d731de0a62143b280ab
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-04 20:04:26 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enables React strict mode for better error detection
+};
+
+module.exports = nextConfig;
