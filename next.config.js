1adda30b1591ced185e43eb9e9a9128187b77818
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-12 20:27:33 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Enables React strict mode for better error detection
-};
-
-module.exports = nextConfig;
